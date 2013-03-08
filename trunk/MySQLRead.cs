93425e90831abc3299329d1d6389ae6aed117cf6
bittiez@gmail.com <bittiez@gmail.com@60bf893d-ec1c-7a86-7e9f-654a244221f3>
2013-03-08 14:35:11 +0000

    Reset the SVN.

---
+++ trunk/MySQLRead.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Server;
+using Server.Commands;
+using System.Net;
+
+
+namespace Bittiez.MYSQL_Saver
+{
+    public class MySQLRead
+    {
+
+        public static void Initialize()
+        {
+            CommandSystem.Register("[readmy", AccessLevel.Seer, new CommandEventHandler(MySQLRead_OnCommand));
+            Console.Write("Mysql Read Loaded.");
+            Console.WriteLine();
+        }
+        [Usage("readmy [description]")]
+        [Description("Attempts to save to mysql.")]
+        public static void MySQLRead_OnCommand(CommandEventArgs e)
+        {
+            MySQLConData ConData = new MySQLConData();
+            MySQLRead my = new MySQLRead();
+
+            MySqlConnection con = ConData.Open_Connect(MySQLConData.db);
+
+            e.Mobile.SendMessage(my.readString(con, e.Mobile.Serial, e.ArgString, 1));
+
+            ConData.Close_Connection(con);
+
+            e.Mobile.SendMessage("Done.");
+        }
+
+
+        public string readString(MySqlConnection con, Serial serial, string return_if_no_data, int Identifier)
+        {
+            string type = "string", sql, stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = (string)dataReader["string"];
+                    int id = (int)dataReader["id"];
+                    dataReader.Close();
+                }
+                if (stringg.Length < 1) return return_if_no_data;
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public DateTime readDateTime(MySqlConnection con, Serial serial, DateTime return_if_no_data, int Identifier)
+        {
+            string type = "dateTime", sql;
+            DateTime stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = new DateTime((long)dataReader["dateTime"]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public IPAddress readIPAddress(MySqlConnection con, Serial serial, IPAddress return_if_no_data, int Identifier)
+        {
+            string type = "ipaddress", sql;
+            IPAddress stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = new IPAddress((long)dataReader[type]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public int readInt(MySqlConnection con, Serial serial, int return_if_no_data, int Identifier)
+        {
+            string type = "int", sql;
+            int stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = ((int)dataReader[type]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public TimeSpan readTimeSpan(MySqlConnection con, Serial serial, TimeSpan return_if_no_data, int Identifier)
+        {
+            string type = "timespan", sql;
+            TimeSpan stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = new TimeSpan((long)dataReader[type]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public decimal readDecimal(MySqlConnection con, Serial serial, decimal return_if_no_data, int Identifier)
+        {
+            string type = "decimal", sql;
+            decimal stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = ((decimal)dataReader[type]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+
+        public long readLong(MySqlConnection con, Serial serial, long return_if_no_data, int Identifier)
+        {
+            string type = "long", sql;
+            long stringg = return_if_no_data;
+
+            try
+            {
+                sql = "SELECT * FROM `" + MySQLConData.database + "`.`" + type + "` WHERE Iden='" + Identifier + "' AND serial='" + serial.Value + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    stringg = ((long)dataReader[type]);
+                    dataReader.Close();
+                }
+                return stringg;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return return_if_no_data;
+            }
+        }
+    }
+}
---
+++ trunk/runuo.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 08, 2013 at 02:25 PM
+-- Server version: 5.6.10
+-- PHP Version: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `runuo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datetime`
+--
+
+CREATE TABLE IF NOT EXISTS `datetime` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `dateTime` bigint(20) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=56 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `decimal`
+--
+
+CREATE TABLE IF NOT EXISTS `decimal` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `decimal` decimal(65,30) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `int`
+--
+
+CREATE TABLE IF NOT EXISTS `int` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `int` int(255) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ipaddress`
+--
+
+CREATE TABLE IF NOT EXISTS `ipaddress` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `ipaddress` bigint(20) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `long`
+--
+
+CREATE TABLE IF NOT EXISTS `long` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `long` bigint(40) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `string`
+--
+
+CREATE TABLE IF NOT EXISTS `string` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `string` longtext NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=56 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timespan`
+--
+
+CREATE TABLE IF NOT EXISTS `timespan` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `serial` int(255) NOT NULL,
+  `timespan` bigint(20) NOT NULL,
+  `Iden` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
